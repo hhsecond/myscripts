52a93c64e8bae66e92ede2da21e85abb20b28849
hhsecond <sherinct@live.com>
2016-12-24 18:33:05 +0530

    notification module for ubuntu

---
+++ notify_me/notifications.py
@@ -0,0 +1,36 @@
+import notify2
+import datetime
+import random
+import time
+import pygame
+
+
+def hey_notify(*args, sleep_fact):
+    n = notify2.Notification(*args)
+    n.show()
+    pygame.mixer.music.play()
+    time.sleep(sleep_fact * mul_fact)
+
+
+notify2.init('foo')
+pygame.mixer.init()
+pygame.mixer.music.load("/home/hhsecond/notify_me/monotone.mp3")
+mul_fact = 60 * 60
+hours = 0
+things_to_do = {'Advanced algorithims and Datastructures': 1,
+                'Research on new trends in AI': 1,
+                'Mathematics is the foundation of science': 1,
+                'CS graduation': 1,
+                'Call your friend': 0.25
+                }
+while things_to_do:
+    random_hour = random.choice([1, 2, 1.5, 2.5])
+    hey_notify('Get Back To Work!!!!', random_hour)
+    current_time = datetime.datetime.now()
+    right_now_item = random.choice(list(things_to_do.keys()))
+    hours = things_to_do[right_now_item]
+    end_time = current_time + datetime.timedelta(hours=hours)
+    hey_notify(right_now_item, 'Do it till ' + end_time.strftime('%r'), hours)
+    del things_to_do[right_now_item]
+
+hey_notify('Take breaks in between and work the shit until you die')
